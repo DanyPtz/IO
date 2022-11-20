bcb0326f0c65d1e576b1ac1aca73551a7f69ade7
DanyPtz <113537213+DanyPtz@users.noreply.github.com>
2022-11-20 19:03:54 +0100

    Add files via upload

---
+++ IO/IO/IOApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOApp.swift
+//  IO
+//
+//  Created by Daniele Patrizio on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct IOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
